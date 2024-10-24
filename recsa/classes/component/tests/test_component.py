b8857dbfa7467c0b0f28f92c5c8ace577d5df33f
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-10-24 11:07:12 +0900

    Refactor test_component_structure.py to test_component.py

--- recsa/classes/component/tests/test_component.py
+++ recsa/classes/component/tests/test_component.py
@@ -1,30 +1,30 @@
 import pytest
 
-from recsa import ComponentStructure, LocalAuxEdge, RecsaValueError
+from recsa import Component, LocalAuxEdge, RecsaValueError
 
 
 @pytest.fixture
-def comp() -> ComponentStructure:
-    return ComponentStructure('M', {'a', 'b'})
+def comp() -> Component:
+    return Component('M', {'a', 'b'})
 
 @pytest.fixture
-def comp_with_aux_edges() -> ComponentStructure:
-    return ComponentStructure('M', {'a', 'b'}, {LocalAuxEdge('a', 'b', 'cis')})
+def comp_with_aux_edges() -> Component:
+    return Component('M', {'a', 'b'}, {LocalAuxEdge('a', 'b', 'cis')})
 
 
 def test_init_with_valid_args(comp) -> None:
-    assert comp.component_kind == 'M'
-    assert set(comp.binding_sites) == {'a', 'b'}
+    assert comp.kind == 'M'
+    assert set(comp.bindsites) == {'a', 'b'}
 
 
 def test_init_with_valid_args_with_single_aux_edge(comp_with_aux_edges) -> None:
-    assert comp_with_aux_edges.component_kind == 'M'
-    assert set(comp_with_aux_edges.binding_sites) == {'a', 'b'}
+    assert comp_with_aux_edges.kind == 'M'
+    assert set(comp_with_aux_edges.bindsites) == {'a', 'b'}
     assert comp_with_aux_edges.aux_edges == {LocalAuxEdge('a', 'b', 'cis')}
 
 
 def test_init_with_valid_args_with_multiple_aux_edges() -> None:
-    component = ComponentStructure('M', {'a', 'b', 'c'}, {
+    component = Component('M', {'a', 'b', 'c'}, {
         LocalAuxEdge('a', 'b', 'cis'), LocalAuxEdge('a', 'c', 'cis'), 
         LocalAuxEdge('b', 'c', 'trans')})
     
@@ -37,38 +37,30 @@ def test_init_with_valid_args_with_multiple_aux_edges() -> None:
 
 def test_init_with_invalid_aux_edge_whose_binding_sites_not_in_binding_sites() -> None:
     with pytest.raises(RecsaValueError):
-        ComponentStructure('M', {'a', 'b'}, {LocalAuxEdge('a', 'c', 'cis')})
+        Component('M', {'a', 'b'}, {LocalAuxEdge('a', 'c', 'cis')})
 
 
 def test_init_with_empty_binding_sites() -> None:
     # Empty binding sites is allowed.
-    component = ComponentStructure('M', set())
+    component = Component('M', set())
     assert component.bindsites == set()
 
 
 def test_init_with_empty_aux_edges() -> None:
     # Empty aux_edges is allowed.
-    component = ComponentStructure('M', {'a', 'b'}, set())
+    component = Component('M', {'a', 'b'}, set())
     assert component.aux_edges == set()
 
 
 def test_eq() -> None:
-    component1 = ComponentStructure('M', {'a', 'b'})
-    component2 = ComponentStructure('M', {'a', 'b'})
-    component3 = ComponentStructure('M', {'a', 'b'}, {LocalAuxEdge('a', 'b', 'cis')})
-    component4 = ComponentStructure('M', {'a', 'b'}, {LocalAuxEdge('a', 'b', 'trans')})
+    component1 = Component('M', {'a', 'b'})
+    component2 = Component('M', {'a', 'b'})
+    component3 = Component('M', {'a', 'b'}, {LocalAuxEdge('a', 'b', 'cis')})
+    component4 = Component('M', {'a', 'b'}, {LocalAuxEdge('a', 'b', 'trans')})
 
     assert component1 == component2
     assert component1 != component3
     assert component3 != component4
-
-
-def test_clear_g_cache(comp) -> None:
-    comp._Component__g_cache = 1
-    assert comp._Component__g_cache == 1
-
-    comp._ComponentStructure__add_binding_site('c')
-    assert comp._ComponentStructure__g_cache is None
 
 
 if __name__ == '__main__':
