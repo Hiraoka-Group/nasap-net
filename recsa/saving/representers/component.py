c79e83c92fb74bb81a3ebff09cc31db72e4b218b
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-10-25 16:39:16 +0900

    Refactor component_representer to sort bindsites before representing

--- recsa/saving/representers/component.py
+++ recsa/saving/representers/component.py
@@ -10,7 +10,7 @@ add_aux_edge_representer()
 def component_representer(dumper, data: Component):
     component_dict = {
         'id': data.kind,
-        'bindsites': list(data.bindsites),
+        'bindsites': sorted(data.bindsites),
         'aux_edges': list(data.aux_edges),
     }
     return dumper.represent_dict(component_dict)
