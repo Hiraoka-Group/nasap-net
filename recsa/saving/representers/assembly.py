af411d6bd8b46f1781f1df2bbcfef68b79d5c1e8
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-10-25 14:39:33 +0900

    Refactor assembly representer to use 'component_structures' instead of 'comp_kind_to_structure'

--- recsa/saving/representers/assembly.py
+++ recsa/saving/representers/assembly.py
@@ -9,7 +9,7 @@ add_component_representer()
 
 def assembly_representer(dumper, data: Assembly):
     assembly_dict = {
-        'comp_kind_to_structure': dict(data.comp_kind_to_structure),
+        'component_structures': dict(data.comp_kind_to_structure),
         'component_id_to_kind': dict(data.component_id_to_kind),
         'bonds': [
             sorted([u, v]) for u, v 
