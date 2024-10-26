ff36d0af9e6350fb1b2a7b3b18b842db406b9ef3
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-10-26 17:45:55 +0900

    Add load_assemblies_in_one_file function and its tests

--- recsa/loading/__init__.py
+++ recsa/loading/__init__.py
@@ -1,5 +1,6 @@
 from .assembly import load_assembly
 from .assembly_multi import load_assemblies
+from .assembly_multi_in_one import load_assemblies_in_one_file
 from .bondsets import load_bondsets
 from .components import load_component_structures
 from .reaction import load_reaction
---
+++ recsa/loading/tests/test_assembly_mlti_in_one.py
@@ -0,0 +1,84 @@
+from collections.abc import Iterable
+from typing import Any
+
+import pytest
+import yaml
+
+from recsa import Assembly, assembly_equality, load_assemblies_in_one_file
+
+
+# Helper function
+def write_safe_data_to_file(tmp_path, data: Iterable[Any]):
+    file = tmp_path / 'assemblies.yaml'
+    with file.open('w') as f:
+        yaml.safe_dump_all(data, f)
+    return file
+
+
+def test_typical_case(tmp_path):
+    data = [
+        {'id': 'MX2',
+         'comp_id_to_kind': {'M1': 'M', 'X1': 'X', 'X2': 'X'},
+         'bonds': [['M1.a', 'X1.a'], ['M1.b', 'X2.a']]},
+        {'id': 'L', 'comp_id_to_kind': {'L1': 'L'}}
+    ]
+    assemblies_file = write_safe_data_to_file(tmp_path, data)
+    
+    loaded = load_assemblies_in_one_file(assemblies_file)
+
+    assert list(loaded) == [
+        ('MX2', Assembly(
+            comp_id_to_kind={'M1': 'M', 'X1': 'X', 'X2': 'X'},
+            bonds=[('M1.a', 'X1.a'), ('M1.b', 'X2.a')])),
+        ('L', Assembly(comp_id_to_kind={'L1': 'L'}, bonds=[]))
+    ]
+
+
+def test_basic(tmp_path):
+    data = [{'comp_id_to_kind': {'M1': 'M'}}]
+    assemblies_file = write_safe_data_to_file(tmp_path, data)
+    loaded = load_assemblies_in_one_file(assemblies_file)
+    assert list(loaded) == [
+        (None, Assembly(comp_id_to_kind={'M1': 'M'}, bonds=[]))]
+
+
+def test_id(tmp_path):
+    data = [{'id': 'freeM', 'comp_id_to_kind': {'M1': 'M'}}]
+    assemblies_file = write_safe_data_to_file(tmp_path, data)
+    loaded = load_assemblies_in_one_file(assemblies_file)
+    assert list(loaded) == [
+        ('freeM', Assembly(comp_id_to_kind={'M1': 'M'}, bonds=[]))]
+
+
+def test_without_comp_id_to_kind(tmp_path):
+    data = [{}]  # type: ignore
+    assemblies_file = write_safe_data_to_file(tmp_path, data)
+    loaded = load_assemblies_in_one_file(assemblies_file)
+    assert list(loaded) == [(None, Assembly(comp_id_to_kind={}, bonds=[]))]
+
+
+def test_empty_comp_id_to_kind(tmp_path):
+    data = [{'comp_id_to_kind': {}}]  # type: ignore
+    assemblies_file = write_safe_data_to_file(tmp_path, data)
+    loaded = load_assemblies_in_one_file(assemblies_file)
+    assert list(loaded) == [(None, Assembly(comp_id_to_kind={}, bonds=[]))]
+
+
+def test_without_bonds(tmp_path):
+    data = [{'comp_id_to_kind': {'M1': 'M'}}]
+    assemblies_file = write_safe_data_to_file(tmp_path, data)
+    loaded = load_assemblies_in_one_file(assemblies_file)
+    assert list(loaded) == [
+        (None, Assembly(comp_id_to_kind={'M1': 'M'}, bonds=[]))]
+
+
+def test_empty_bonds(tmp_path):
+    data = [{'comp_id_to_kind': {'M1': 'M'}, 'bonds': []}]
+    assemblies_file = write_safe_data_to_file(tmp_path, data)
+    loaded = load_assemblies_in_one_file(assemblies_file)
+    assert list(loaded) == [
+        (None, Assembly(comp_id_to_kind={'M1': 'M'}, bonds=[]))]
+
+
+if __name__ == '__main__':
+    pytest.main(['-vv', __file__])
