4caec39ada08bde1d6b9a7e705644474a85ab9b8
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-10-26 11:46:15 +0900

    Refactor class name LocalAuxEdge to AuxEdge

--- recsa/algorithms/isomorphism/tests/test_isomorphism_check.py
+++ recsa/algorithms/isomorphism/tests/test_isomorphism_check.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 
 import pytest
 
-from recsa import Assembly, ComponentStructure, LocalAuxEdge, is_isomorphic
+from recsa import Assembly, AuxEdge, ComponentStructure, is_isomorphic
 
 
 @pytest.fixture
@@ -19,8 +19,8 @@ def component_structures() -> dict[str, ComponentStructure]:
     M_COMP = ComponentStructure(
         'M', {'a', 'b', 'c', 'd'},
         {
-            LocalAuxEdge('a', 'b', 'cis'), LocalAuxEdge('b', 'c', 'cis'),
-            LocalAuxEdge('c', 'd', 'cis'), LocalAuxEdge('d', 'a', 'cis'),})
+            AuxEdge('a', 'b', 'cis'), AuxEdge('b', 'c', 'cis'),
+            AuxEdge('c', 'd', 'cis'), AuxEdge('d', 'a', 'cis'),})
     L_COMP = ComponentStructure('L', {'a', 'b'})
     X_COMP = ComponentStructure('X', {'a'})
     return {'M': M_COMP, 'L': L_COMP, 'X': X_COMP}
--- recsa/assembly_drawing/tests/test_drawing_2d.py
+++ recsa/assembly_drawing/tests/test_drawing_2d.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import pytest
 
-from recsa import Assembly, ComponentStructure, LocalAuxEdge, draw_2d
+from recsa import Assembly, AuxEdge, ComponentStructure, draw_2d
 
 
 @pytest.mark.skip(reason='This is a visual test.')
@@ -13,8 +13,8 @@ def test_draw_2d():
     COMPONENT_STRUCTURES = {
         'M': ComponentStructure(
             'M', {'a', 'b', 'c', 'd'}, {
-                LocalAuxEdge('a', 'b', 'cis'), LocalAuxEdge('b', 'c', 'cis'),
-                LocalAuxEdge('c', 'd', 'cis'), LocalAuxEdge('d', 'a', 'cis')}),
+                AuxEdge('a', 'b', 'cis'), AuxEdge('b', 'c', 'cis'),
+                AuxEdge('c', 'd', 'cis'), AuxEdge('d', 'a', 'cis')}),
         'L': ComponentStructure('L', {'a', 'b'}),
         'X': ComponentStructure('X', {'a'}),
     }
