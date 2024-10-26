41642504ca0c349ea0302bb5039ce30cfbd4b18c
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-10-26 15:04:09 +0900

    Refactor load_component_structures not to use pydantic

--- recsa/loading/component_structures.py
+++ recsa/loading/component_structures.py
@@ -1,66 +1,57 @@
 from pathlib import Path
-
-import yaml
-from pydantic.dataclasses import dataclass
+from typing import Any
 
 from recsa import AuxEdge, Component
+
+from .yaml_safe_load import load_yaml_safely
 
 __all__ = ['load_component_structures']
 
 
 def load_component_structures(
-        file_path: str | Path
-        ) -> dict[str, Component]:
+        file_path: str | Path) -> dict[str, Component]:
     """Load ComponentStructure objects from a YAML file."""
     file_path = Path(file_path)
-    data = load_yaml(file_path)
+    data = load_yaml_safely(file_path)
     return create_component_structures_from_data(data)
 
 
-@dataclass
-class AuxEdgeData:
-    bindsites: tuple[str, str]
-    kind: str
-
-
-@dataclass
-class ComponentStructureData:
-    id: str
-    bindsites: list[str]
-    aux_edges: list[AuxEdgeData] | None = None
-
-
-@dataclass
-class ComponentStructuresData:
-    component_structures: list[ComponentStructureData]
-
-
-def load_yaml(file_path: str | Path) -> list[ComponentStructureData]:
-    """Load YAML file and return the data as a dictionary."""
-    with open(file_path, 'r') as file:
-        data = yaml.safe_load(file)
-    
-    component_structures = []
-    for component_structure in data['component_structures']:
-        component_structures.append(
-            ComponentStructureData(**component_structure))
-    return component_structures
-
-
 def create_component_structures_from_data(
-        data: list[ComponentStructureData]
-        ) -> dict[str, Component]:
+        data: Any) -> dict[str, Component]:
     """Create ComponentStructure objects from a dictionary."""
-    component_structures = {}
-    for component_structure in data:
-        bindsites = set(component_structure.bindsites)
-        aux_edges = set()
-        if component_structure.aux_edges:
-            if component_structure.aux_edges:
-                for aux_edge in component_structure.aux_edges:
-                    bindsite1, bindsite2 = aux_edge.bindsites
-                    kind = aux_edge.kind
-                    aux_edges.add(AuxEdge(bindsite1, bindsite2, kind))
-        component_structures[component_structure.id] = Component(
-            bindsites, aux_edges)
-    return component_structures
+    # Expected format:
+    # {'components': {
+    #     'M': {
+    #         'bindsites': ['a', 'b'],
+    #         'aux_edges': [['a', 'b', 'cis']]
+    #     },
+    #     'X': {
+    #         'bindsites': ['a'],
+    #     }
+    # }}
+
+    if not isinstance(data, dict):
+        raise ValueError('Expected a dictionary')
+    if 'components' not in data:
+        raise ValueError('Expected a key "component_structures"')
+    
+    comp_kind_to_data = data['components']
+    if not isinstance(comp_kind_to_data, dict):
+        raise ValueError('Expected a dictionary')
+    
+    comp_kind_to_obj = {}
+
+    for comp_kind, comp_data in comp_kind_to_data.items():
+        bindsites_data = comp_data.get('bindsites', [])
+        bindsites_data = [str(bind) for bind in bindsites_data]
+
+        aux_edges_data = comp_data.get('aux_edges', [])
+        aux_edges_data = [
+            AuxEdge(*aux_param) for aux_param in aux_edges_data]
+        
+        comp_kind_to_obj[comp_kind] = Component(
+            bindsites=bindsites_data,
+            aux_edges=aux_edges_data
+        )
+    
+    return comp_kind_to_obj
--- recsa/loading/tests/test_component_structures.py
+++ recsa/loading/tests/test_component_structures.py
@@ -1,10 +1,56 @@
 import pytest
+import yaml
 
-from recsa import load_component_structures
+from recsa import Component, load_component_structures
 
 
-def test_load_component_structures():
-    pass
+# Helper function
+def write_safe_data_to_file(tmp_path, data):
+    file = tmp_path / 'components.yaml'
+    with file.open('w') as f:
+        yaml.safe_dump(data, f)
+    return file
+
+
+def test_typical_case(tmp_path):
+    COMPONENTS = {
+        'M': {'bindsites': ['a', 'b'],
+              'aux_edges': [['a', 'b', 'cis']]},
+        'X': {'bindsites': ['a'],}
+    }
+    data = {'components': COMPONENTS}
+
+    component_structures_file = write_safe_data_to_file(tmp_path, data)
+    
+    loaded = load_component_structures(component_structures_file)
+
+    assert loaded == {
+        'M': Component(bindsites=['a', 'b'], 
+                       aux_edges=[('a', 'b', 'cis')]),
+        'X': Component(bindsites=['a'], aux_edges=[])
+    }
+
+
+def test_component_without_bindsites(tmp_path):
+    COMPONENTS: dict[str, dict] = {'M': {}}
+    data = {'components': COMPONENTS}
+
+    component_structures_file = write_safe_data_to_file(tmp_path, data)
+    
+    loaded = load_component_structures(component_structures_file)
+
+    assert loaded == {'M': Component(bindsites=[], aux_edges=[])}
+
+
+def test_component_without_aux_edges(tmp_path):
+    COMPONENTS: dict[str, dict] = {'M': {'bindsites': ['a']}}
+    data = {'components': COMPONENTS}
+
+    component_structures_file = write_safe_data_to_file(tmp_path, data)
+    
+    loaded = load_component_structures(component_structures_file)
+
+    assert loaded == {'M': Component(bindsites=['a'], aux_edges=[])}
 
 
 if __name__ == '__main__':
