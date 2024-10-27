8224707e40cec191c9c611a8dd5472ff1032d1ce
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-10-27 14:01:17 +0900

    Refactor get_connected_num function to use param name source_component instead of component

--- recsa/reaction_classification/utils/bindsite_num.py
+++ recsa/reaction_classification/utils/bindsite_num.py
@@ -4,11 +4,11 @@ from recsa import Assembly, BindsiteIdConverter, Component
 def get_connected_num(
         assembly: Assembly, source_comp_id: str,
         target_comp_kind: str,
-        component: Component
+        source_component: Component
         ) -> int:
     id_converter = BindsiteIdConverter()
     num = 0
-    for bindsite in component.bindsites:
+    for bindsite in source_component.bindsites:
         global_bindsite = id_converter.local_to_global(
             source_comp_id, bindsite)
         connected = assembly.get_connected_bindsite(global_bindsite)
