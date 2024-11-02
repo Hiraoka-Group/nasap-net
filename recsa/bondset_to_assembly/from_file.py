3554cc8ab70994b1d59e39450d73af3492663421
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-11-02 19:13:31 +0900

    Adjust convert_bondsets_to_assemblies_from_file

--- recsa/bondset_to_assembly/from_file.py
+++ recsa/bondset_to_assembly/from_file.py
@@ -8,9 +8,9 @@ __all__ = ['convert_bondsets_to_assemblies_from_file']
 
 def convert_bondsets_to_assemblies_from_file(
         bond_list_path: str, template_path: str) -> Iterable[Assembly]:
-    assems_by_bond_list = load_bondsets(bond_list_path)
+    id_to_bondset = load_bondsets(bond_list_path)
     args = load_structure_data(template_path)
 
-    for bondset in assems_by_bond_list:
+    for bondset in id_to_bondset.values():
         yield convert_bondset_to_assembly(
             set(bondset), args.components, args.bond_id_to_bindsites)
