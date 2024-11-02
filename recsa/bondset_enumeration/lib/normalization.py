96f1951217d5c9c2d8c5c1bc40ea670ca0eb3e67
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-11-02 12:25:06 +0900

    Refactor bondset normalization

--- recsa/bondset_enumeration/lib/normalization.py
+++ recsa/bondset_enumeration/lib/normalization.py
@@ -1,12 +1,12 @@
-from collections.abc import Mapping
+from collections.abc import Iterable, Mapping
 
 from .symmetry_application import apply_symmetry_operation
 
-__all__ = ['find_representative_under_sym_ops']
+__all__ = ['normalize_bondset_under_sym_ops']
 
 
-def find_representative_under_sym_ops(
-        assembly: set[str],
+def normalize_bondset_under_sym_ops(
+        bondset: Iterable[str],
         sym_ops: Mapping[str, Mapping[str, str]]
         ) -> set[str]:
     """Find a representative of an assembly under symmetry operations.
@@ -17,8 +17,9 @@ def find_representative_under_sym_ops(
 
     # TODO: Add an example.
     """
+    bondset = set(bondset)
     for sym_op in sym_ops.values():
-        transformed_assem = apply_symmetry_operation(assembly, sym_op)
-        if transformed_assem < assembly:
-            return transformed_assem
-    return assembly
+        transformed_assem = apply_symmetry_operation(bondset, sym_op)
+        if sorted(transformed_assem) < sorted(bondset):
+            bondset = transformed_assem
+    return bondset
