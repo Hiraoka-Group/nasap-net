5984a1af72e1c0376ac77bd2bd9869f329a16438
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-11-05 13:58:28 +0900

    Remove unused `assembly_enumeration` subpackage

--- recsa/assembly_enumeration/core.py
+++
@@ -1,7 +0,0 @@
-from recsa import RecsaNotImplementedError
-
-__all__ = ['enumerate_assemblies']
-
-
-def enumerate_assemblies():
-    raise RecsaNotImplementedError('enumerate_assemblies')
--- recsa/assembly_enumeration/from_file.py
+++
@@ -1,7 +0,0 @@
-from recsa import RecsaNotImplementedError
-
-__all__ = ['enumerate_assemblies_from_file']
-
-
-def enumerate_assemblies_from_file():
-    raise RecsaNotImplementedError('enumerate_assemblies')
