588b554877cbcf5832243812532d1504703e88c0
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-11-05 12:35:40 +0900

    Remove `save_reaction`, `save_reactions` and relevant representers

--- recsa/saving/__init__.py
+++ recsa/saving/__init__.py
@@ -2,7 +2,6 @@ from .assemblies import *
 from .bondset_txt import *
 from .bondsets import *
 from .components import *
-from .reaction import *
 from .reactions_df_conversion import *
 from .representers import *
 from .utils import *
