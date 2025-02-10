a46b71e1726bc92fa7681b06d055d48fcafcb2ea
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-02-10 11:20:59 +0900

    Refactor bondset functions to use generic types for improved flexibility

--- recsa/bondset_enumeration/lib/single_bond.py
+++ recsa/bondset_enumeration/lib/single_bond.py
@@ -1,18 +1,21 @@
-from collections.abc import Iterable, Mapping
+from collections.abc import Hashable, Iterable, Mapping
+from typing import TypeVar
 
 from .is_new_check import is_new_under_symmetry
 
 __all__ = ['enum_single_bond_subsets']
 
+_T = TypeVar('_T', bound=Hashable)
+
 
 def enum_single_bond_subsets(
-        bonds: Iterable[int],
-        sym_ops: Mapping[str, Mapping[int, int]] | None = None
-        ) -> set[frozenset[int]]:
+        bonds: Iterable[_T],
+        sym_ops: Mapping[str, Mapping[_T, _T]] | None = None
+        ) -> set[frozenset[_T]]:
     """Enumerate single-bond subsets of bonds 
     excluding disconnected ones and symmetry-equivalent ones.
     """
-    found: set[frozenset[int]] = set()
+    found: set[frozenset[_T]] = set()
 
     # NOTE: The order of the iteration should be fixed to make the
     # result deterministic.
