258c944ee6bb2f71c5aa81931f1e26646bdcb98f
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-02-18 15:12:07 +0900

    Add pipeline subpackage and implement enum_bond_subsets_pipeline

---
+++ recsa/pipelines/lib/saving.py
@@ -0,0 +1,23 @@
+import os
+from typing import Any
+
+import yaml
+
+
+def write_output(
+        output_path: os.PathLike | str, data: Any,
+        *,
+        overwrite: bool = False,
+        default_flow_style: bool | None = None,
+        verbose: bool = False
+        ) -> None:
+    """Write the output data to a file using YAML format."""
+    if not overwrite and os.path.exists(output_path):
+        raise FileExistsError(f'Output file "{output_path}" already exists.')
+    if verbose:
+        print(f'Saving the results to "{output_path}"...')
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, 'w') as f:
+        yaml.safe_dump(data, f, default_flow_style=default_flow_style)
+    if verbose:
+        print(f'Successfully saved the results to "{output_path}".')
