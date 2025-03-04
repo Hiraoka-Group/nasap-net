b09ecb08b22bc79ac08f0d7a89b8e003b4940fad
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-03-04 11:01:55 +0900

    Add bondset_to_assembly command to CLI

--- recsa/cli/commands.py
+++ recsa/cli/commands.py
@@ -1,6 +1,7 @@
 import click
 
-from recsa.pipelines import enum_bond_subsets_pipeline
+from recsa.pipelines import (bondsets_to_assemblies_pipeline,
+                             enum_bond_subsets_pipeline)
 
 
 @click.command()
@@ -12,3 +13,17 @@ from recsa.pipelines import enum_bond_subsets_pipeline
     help='Output file path')
 def run_bond_subset_pipeline(input, output):
     enum_bond_subsets_pipeline(input, output)
+
+
+@click.command()
+@click.option(
+    '--bondsets', '-b', type=click.Path(exists=True), required=True,
+    help='Path to bond subsets file')
+@click.option(
+    '--structure', '-s', type=click.Path(exists=True), required=True,
+    help='Path to structure data file')
+@click.option(
+    '--output', '-o', type=click.Path(), required=True,
+    help='Output file path')
+def run_bondset_to_assembly_pipeline(bondsets, structure, output):
+    bondsets_to_assemblies_pipeline(bondsets, structure, output)
--- recsa/cli/main.py
+++ recsa/cli/main.py
@@ -1,6 +1,7 @@
 import click
 
-from recsa.cli.commands import run_bond_subset_pipeline
+from recsa.cli.commands import (run_bond_subset_pipeline,
+                                run_bondset_to_assembly_pipeline)
 
 
 @click.group()
@@ -9,6 +10,7 @@ def main():
     pass
 
 main.add_command(run_bond_subset_pipeline, name='enum-bond-subsets')
+main.add_command(run_bondset_to_assembly_pipeline, name='bondset-to-assembly')
 
 if __name__ == '__main__':
     main()
