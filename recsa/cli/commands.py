54dc1ff6bdada788ffbdb8553a229e0a91951e0e
Keisuke Takeuchi <142223934+neji-craftsman@users.noreply.github.com>
2025-03-05 08:36:13 +0900

    V0.6.0 (#132)
    * Refactor CLI commands to use arguments instead of options (#131)
    
    * Refactor CLI commands to use arguments instead of options
    
    * Rename run_enum_bond_subset_pipeline to run_enum_bond_subsets_pipeline for consistency
    
    * Bump version to 0.6.0 in pyproject.toml

--- recsa/cli/commands.py
+++ recsa/cli/commands.py
@@ -4,26 +4,33 @@ from recsa.pipelines import (bondsets_to_assemblies_pipeline,
                              enum_bond_subsets_pipeline)
 
 
-@click.command()
-@click.option(
-    '--input', '-i', type=click.Path(exists=True), required=True, 
-    help='Input file path')
-@click.option(
-    '--output', '-o', type=click.Path(), required=True, 
-    help='Output file path')
-def run_bond_subset_pipeline(input, output):
+@click.command('enumerate-bond-subsets')
+@click.argument('input', type=click.Path(exists=True))
+@click.argument('output', type=click.Path())
+def run_enum_bond_subsets_pipeline(input, output):
+    """Enumerates bond subsets.
+    
+    \b
+    Parameters
+    ----------
+    - INPUT: Path to input file.
+    - OUTPUT: Path to output file.
+    """
     enum_bond_subsets_pipeline(input, output)
 
 
-@click.command()
-@click.option(
-    '--bondsets', '-b', type=click.Path(exists=True), required=True,
-    help='Path to bond subsets file')
-@click.option(
-    '--structure', '-s', type=click.Path(exists=True), required=True,
-    help='Path to structure data file')
-@click.option(
-    '--output', '-o', type=click.Path(), required=True,
-    help='Output file path')
-def run_bondset_to_assembly_pipeline(bondsets, structure, output):
+@click.command('bondsets-to-assemblies')
+@click.argument('bondsets', type=click.Path(exists=True))
+@click.argument('structure', type=click.Path(exists=True))
+@click.argument('output', type=click.Path())
+def run_bondsets_to_assemblies_pipeline(bondsets, structure, output):
+    """Converts bondsets to assemblies.
+    
+    \b
+    Parameters
+    ----------
+    - BONDSETS: Path to input file of bond subsets.
+    - STRUCTURE: Path to input file of structure.
+    - OUTPUT: Path to output file.
+    """
     bondsets_to_assemblies_pipeline(bondsets, structure, output)
