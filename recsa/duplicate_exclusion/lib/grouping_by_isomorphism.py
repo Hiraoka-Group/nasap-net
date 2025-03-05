f6b8bbcdb808df6b1081776d873b4f63c5b533ff
Keisuke Takeuchi <142223934+neji-craftsman@users.noreply.github.com>
2025-03-05 14:00:01 +0900

    Release/v0.6.1 (#136)
    * Refactor CLI commands to use arguments instead of options (#131)
    
    * Refactor CLI commands to use arguments instead of options
    
    * Rename run_enum_bond_subset_pipeline to run_enum_bond_subsets_pipeline for consistency
    
    * Typing/generic (#133)
    
    * Refactor `group_assemblies_by_hash` to use generic type `_T`
    
    * Refactor `group_assemblies_by_isomorphism` to use generic type `_T`
    
    * Update type hints in find_unique_assemblies to use Sequence (#134)
    
    * Fix log message in bondsets_to_assemblies_pipeline (#135)
    
    * Bump version to 0.6.1 in pyproject.toml

--- recsa/duplicate_exclusion/lib/grouping_by_isomorphism.py
+++ recsa/duplicate_exclusion/lib/grouping_by_isomorphism.py
@@ -1,5 +1,6 @@
-from collections.abc import Iterable, Mapping
+from collections.abc import Hashable, Iterable, Mapping
 from itertools import combinations
+from typing import TypeVar, cast
 
 from networkx.utils import UnionFind
 
@@ -9,24 +10,24 @@ from .grouping_by_hash import group_assemblies_by_hash
 
 __all__ = ['group_assemblies_by_isomorphism']
 
-
+_T = TypeVar('_T', bound=Hashable)
 
 def group_assemblies_by_isomorphism(
-        id_to_assembly: Mapping[int, Assembly],
+        id_to_assembly: Mapping[_T, Assembly],
         component_structures: Mapping[str, Component]
-        ) -> dict[int, set[int]]:
+        ) -> dict[_T, set[_T]]:
     """Group duplicates by assembly isomorphism.
 
     Parameters
     ----------
-    id_to_grpah : Mapping[str, Assembly]
+    id_to_assembly : Mapping[_T, Assembly]
         A mapping from IDs to assemblies.
     component_structures : Mapping[str, ComponentStructure]
         A mapping from component kinds to component structures.
 
     Returns
     -------
-    dict[str, set[str]]
+    dict[_T, set[_T]]
         A mapping from unique IDs to sets of duplicate IDs.
         Unique IDs are the minimum IDs in each group.
         Duplicate IDs include the unique IDs themselves.
@@ -47,6 +48,6 @@ def group_assemblies_by_isomorphism(
                     component_structures):
                 uf.union(id1, id2)
 
-    grouped_ids: Iterable[set[int]] = uf.to_sets()
+    grouped_ids = cast(Iterable[set[_T]], uf.to_sets())
     unique_id_to_ids = {min(ids): ids for ids in grouped_ids}
     return unique_id_to_ids
