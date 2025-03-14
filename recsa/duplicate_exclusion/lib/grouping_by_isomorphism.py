dc6799505dbb953ab3db09d3ff17ee2017dd793c
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-03-14 16:07:41 +0900

    Add support for predefined non-isomorphic groups in assembly grouping

--- recsa/duplicate_exclusion/lib/grouping_by_isomorphism.py
+++ recsa/duplicate_exclusion/lib/grouping_by_isomorphism.py
@@ -14,7 +14,9 @@ _T = TypeVar('_T', bound=Hashable)
 
 def group_assemblies_by_isomorphism(
         id_to_assembly: Mapping[_T, Assembly],
-        component_structures: Mapping[str, Component]
+        component_structures: Mapping[str, Component],
+        *,
+        non_isomorphic_groups: Iterable[set[_T]] | None = None
         ) -> dict[_T, set[_T]]:
     """Group duplicates by assembly isomorphism.
 
@@ -22,8 +24,13 @@ def group_assemblies_by_isomorphism(
     ----------
     id_to_assembly : Mapping[_T, Assembly]
         A mapping from IDs to assemblies.
-    component_structures : Mapping[str, ComponentStructure]
+    component_structures : Mapping[str, Component]
         A mapping from component kinds to component structures.
+    non_isomorphic_groups : Iterable[set[_T]], optional
+        Predefined groups of assemblies that are guaranteed to be unique
+        and non-isomorphic. Assemblies in the same group will not be 
+        checked for isomorphism. If None, all assemblies are checked for 
+        isomorphism.
 
     Returns
     -------
@@ -31,19 +38,36 @@ def group_assemblies_by_isomorphism(
         A mapping from unique IDs to sets of duplicate IDs.
         Unique IDs are the minimum IDs in each group.
         Duplicate IDs include the unique IDs themselves.
+
+    Notes
+    -----
+    The parameter `non_isomorphic_groups` is useful when there are
+    assemblies that are known to be unique and non-isomorphic. This
+    can be used to reduce the number of isomorphism checks, which can
+    be computationally expensive.
     """
     hash_to_ids = group_assemblies_by_hash(id_to_assembly, component_structures)
+    
+    if non_isomorphic_groups is not None:
+        id_to_non_isomorphic_group = {
+            id_: group for group in non_isomorphic_groups for id_ in group}
 
     uf = UnionFind(id_to_assembly.keys())
 
-    for hash_, ids in hash_to_ids.items():
-        if len(ids) == 1:
+    for hash_, same_hash_ids in hash_to_ids.items():
+        if len(same_hash_ids) == 1:
             continue
 
-        for id1, id2 in combinations(ids, 2):
-            if uf[id1] == uf[id2]:
+        for id1, id2 in combinations(same_hash_ids, 2):
+            if uf[id1] == uf[id2]:  # Already checked to be isomorphic
                 continue
-            if is_isomorphic(
+            if non_isomorphic_groups is not None:
+                non_isom_group1 = id_to_non_isomorphic_group.get(id1)
+                non_isom_group2 = id_to_non_isomorphic_group.get(id2)
+                if non_isom_group1 is not None and non_isom_group1 == non_isom_group2:
+                    # Already guaranteed to be non-isomorphic
+                    continue
+            if is_isomorphic(  # Need isomorphism check
                     id_to_assembly[id1], id_to_assembly[id2],
                     component_structures):
                 uf.union(id1, id2)
