f9fbe515649f24374b13bb69e74dbb6d0a6672b3
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-03-16 11:07:31 +0900

    Remove section_parser utility and its associated tests

--- recsa/utils/__init__.py
+++ recsa/utils/__init__.py
@@ -4,6 +4,5 @@ from .frozen_unordered_pair import *
 from .mapping_iterable_comparison import *
 from .nested_dict_update import *
 from .node_equivalence import *
-from .section_parser import *
 from .uf_to_set_of_frozenset_conversion import *
 from .unique_filepath import *
