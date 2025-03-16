0fe2e8a7669781af9e4ace3fc230165bdceba99f
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-03-16 11:08:00 +0900

    Remove uf_to_set_of_frozenset_conversion utility and its associated tests

--- recsa/utils/__init__.py
+++ recsa/utils/__init__.py
@@ -4,5 +4,4 @@ from .frozen_unordered_pair import *
 from .mapping_iterable_comparison import *
 from .nested_dict_update import *
 from .node_equivalence import *
-from .uf_to_set_of_frozenset_conversion import *
 from .unique_filepath import *
