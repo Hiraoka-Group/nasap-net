0072881f85c1a34630f8e5594b145a246fa4d136
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-04-21 11:11:02 +0900

    Remove bond subset enumeration and bondset to assembly commands from CLI

--- recsa/cli/main.py
+++ recsa/cli/main.py
@@ -1,9 +1,7 @@
 import click
 
-from recsa.cli.commands import (run_bondsets_to_assemblies_pipeline,
-                                run_concat_assembly_lists_pipeline,
-                                run_enum_assemblies_pipeline,
-                                run_enum_bond_subsets_pipeline)
+from recsa.cli.commands import (run_concat_assembly_lists_pipeline,
+                                run_enum_assemblies_pipeline)
 
 
 @click.group()
@@ -12,7 +10,5 @@ def main():
     """RECSA CLI"""
     pass
 
-main.add_command(run_enum_bond_subsets_pipeline)
 main.add_command(run_concat_assembly_lists_pipeline)
-main.add_command(run_bondsets_to_assemblies_pipeline)
 main.add_command(run_enum_assemblies_pipeline)
