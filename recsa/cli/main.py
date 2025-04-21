905ff730c57835192d03aeebe21d71cdaa618328
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2025-04-21 11:07:35 +0900

    Remove unnecessary main guard from CLI entry point

--- recsa/cli/main.py
+++ recsa/cli/main.py
@@ -16,6 +16,3 @@ main.add_command(run_enum_bond_subsets_pipeline)
 main.add_command(run_concat_assembly_lists_pipeline)
 main.add_command(run_bondsets_to_assemblies_pipeline)
 main.add_command(run_enum_assemblies_pipeline)
-
-if __name__ == '__main__':
-    main()
