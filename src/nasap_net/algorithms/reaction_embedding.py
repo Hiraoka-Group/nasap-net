dff1b4648b98e2a303b8afa34753e69774404b31
Keisuke Takeuchi <142223934+takeuchi-sci@users.noreply.github.com>
2025-09-21 15:42:32 +0900

    RichReaction クラスに from_reaction クラスメソッドを追加

--- src/nasap_net/algorithms/reaction_embedding.py
+++ src/nasap_net/algorithms/reaction_embedding.py
@@ -1,43 +1,17 @@
-from typing import overload
-
 from nasap_net import (Assembly, InterReaction, InterReactionRich,
-                       IntraReaction, IntraReactionRich)
+                       IntraReaction, IntraReactionRich, InterOrIntra)
 
 
-@overload
-def embed_assemblies_into_reaction(
-    reaction: IntraReaction,
-    id_to_assembly: dict[int, Assembly]
-    ) -> IntraReactionRich: ...
-@overload
-def embed_assemblies_into_reaction(
-    reaction: InterReaction,
-    id_to_assembly: dict[int, Assembly]
-    ) -> InterReactionRich: ...
 def embed_assemblies_into_reaction(
         reaction: IntraReaction | InterReaction,
-        id_to_assembly: dict[int, Assembly]):
+        id_to_assembly: dict[int, Assembly]
+        ) -> IntraReactionRich | InterReactionRich:
     """Embed the assemblies into the reaction."""
-    init_assem = id_to_assembly[reaction.init_assem_id]
-    product_assem = id_to_assembly[reaction.product_assem_id]
-    
-    if reaction.leaving_assem_id is None:
-        leaving_assem = None
+    if reaction.inter_or_intra == InterOrIntra.INTRA:
+        assert isinstance(reaction, IntraReaction)
+        return IntraReactionRich.from_reaction(
+            reaction, id_to_assembly)
     else:
-        leaving_assem = id_to_assembly[reaction.leaving_assem_id]
-
-    if isinstance(reaction, IntraReaction):
-        return IntraReactionRich(
-            init_assem, product_assem, leaving_assem,
-            reaction.metal_bs, reaction.leaving_bs, reaction.entering_bs,
-            reaction.duplicate_count
-        )
-    elif isinstance(reaction, InterReaction):
-        entering_assem = id_to_assembly[reaction.entering_assem_id]
-        return InterReactionRich(
-            init_assem, entering_assem, product_assem, leaving_assem,
-            reaction.metal_bs, reaction.leaving_bs, reaction.entering_bs,
-            reaction.duplicate_count
-        )
-    else:
-        raise ValueError("Invalid reaction type")
+        assert isinstance(reaction, InterReaction)
+        return InterReactionRich.from_reaction(
+            reaction, id_to_assembly)
--- src/nasap_net/classes/rich_reaction.py
+++ src/nasap_net/classes/rich_reaction.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+from collections.abc import Mapping
 from dataclasses import dataclass
 from functools import cached_property
 from typing import ClassVar
@@ -5,8 +7,35 @@ from typing import ClassVar
 from .assembly import Assembly
 
 
+class RichReactionBase(ABC):
+    @property
+    @abstractmethod
+    def metal_kind(self) -> str:
+        """Kind of the metal component."""
+        pass
+
+    @property
+    @abstractmethod
+    def leaving_kind(self) -> str:
+        """Kind of the leaving component."""
+        pass
+
+    @property
+    @abstractmethod
+    def entering_kind(self) -> str:
+        """Kind of the entering component."""
+        pass
+
+    @classmethod
+    @abstractmethod
+    def from_reaction(cls, reaction, id_to_assembly: Mapping[int, Assembly]):
+        """Create a RichReaction from a Reaction and a mapping from assembly
+        IDs to assemblies."""
+        pass
+
+
 @dataclass(frozen=True)
-class IntraReactionRich:
+class IntraReactionRich(RichReactionBase):
     init_assem: Assembly
     entering_assem: ClassVar[None] = None
     product_assem: Assembly
@@ -31,9 +60,27 @@ class IntraReactionRich:
         return self.init_assem.get_component_kind_of_bindsite(
             self.entering_bs)
 
+    @classmethod
+    def from_reaction(cls, reaction, id_to_assembly: Mapping[int, Assembly]):
+        init_assem = id_to_assembly[reaction.init_assem_id]
+        product_assem = id_to_assembly[reaction.product_assem_id]
+        if reaction.leaving_assem_id is None:
+            leaving_assem = None
+        else:
+            leaving_assem = id_to_assembly[reaction.leaving_assem_id]
+        return cls(
+            init_assem=init_assem,
+            product_assem=product_assem,
+            leaving_assem=leaving_assem,
+            metal_bs=reaction.metal_bs,
+            leaving_bs=reaction.leaving_bs,
+            entering_bs=reaction.entering_bs,
+            duplicate_count=reaction.duplicate_count
+        )
+
 
 @dataclass(frozen=True)
-class InterReactionRich:
+class InterReactionRich(RichReactionBase):
     init_assem: Assembly
     entering_assem: Assembly
     product_assem: Assembly
@@ -57,3 +104,23 @@ class InterReactionRich:
     def entering_kind(self) -> str:
         return self.entering_assem.get_component_kind_of_bindsite(
             self.entering_bs)
+
+    @classmethod
+    def from_reaction(cls, reaction, id_to_assembly: Mapping[int, Assembly]):
+        init_assem = id_to_assembly[reaction.init_assem_id]
+        entering_assem = id_to_assembly[reaction.entering_assem_id]
+        product_assem = id_to_assembly[reaction.product_assem_id]
+        if reaction.leaving_assem_id is None:
+            leaving_assem = None
+        else:
+            leaving_assem = id_to_assembly[reaction.leaving_assem_id]
+        return cls(
+            init_assem=init_assem,
+            entering_assem=entering_assem,
+            product_assem=product_assem,
+            leaving_assem=leaving_assem,
+            metal_bs=reaction.metal_bs,
+            leaving_bs=reaction.leaving_bs,
+            entering_bs=reaction.entering_bs,
+            duplicate_count=reaction.duplicate_count
+        )
