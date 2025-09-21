11b827d7263185f22b41e553182c43b96e3a15e0
Keisuke Takeuchi <142223934+takeuchi-sci@users.noreply.github.com>
2025-09-21 21:05:18 +0900

    import 文の整形

--- src/nasap_net/classes/__init__.py
+++ src/nasap_net/classes/__init__.py
@@ -2,6 +2,7 @@ from .assembly import Assembly
 from .aux_edge import AuxEdge
 from .bindsite_id_converter import BindsiteIdConverter
 from .component import Component
-from .reaction import (InterReaction, IntraReaction,
-                       InterOrIntra)
-from .rich_reaction import IntraReactionRich, InterReactionRich
+from .reaction import (InterOrIntra, InterReaction, IntraReaction,
+                       ReactionBase)
+from .rich_reaction import InterReactionRich, IntraReactionRich, \
+    RichReactionBase
