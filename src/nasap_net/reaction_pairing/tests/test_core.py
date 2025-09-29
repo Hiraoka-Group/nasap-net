886a2f5d45e5de339a4ef6361a1528e836ed834c
Keisuke Takeuchi <142223934+takeuchi-sci@users.noreply.github.com>
2025-09-29 20:37:21 +0900

    [作業中] テストを M2L2-ring の全素反応（おそらく）を用いるように変更

--- src/nasap_net/reaction_pairing/tests/test_core.py
+++ src/nasap_net/reaction_pairing/tests/test_core.py
@@ -22,28 +22,28 @@ def assemblies():
             [('X0.a', 'M0.a'), ('M0.b', 'X1.a')]
         ),
         'L': Assembly({'L0': 'L'}),  # L: (a)L0(b)
-        'X': Assembly({'X0': 'X'}),  # X: (a)X0
         # MLX: (a)L0(b)-(a)M0(b)-(a)X0
         'MLX': Assembly(
             {'M0': 'M', 'L0': 'L', 'X0': 'X'},
             [('L0.b', 'M0.a'), ('M0.b', 'X0.a')]
         ),
+        'X': Assembly({'X0': 'X'}),  # X: (a)X0
         # ML2: (a)L0(b)-(a)M0(b)-(a)L1(b)
         'ML2': Assembly(
             {'M0': 'M', 'L0': 'L', 'L1': 'L'},
             [('L0.b', 'M0.a'), ('M0.b', 'L1.a')]
-        ),
-        # M2L2X: X0(a)-(a)M0(b)-(a)L0(b)-(a)M1(b)-(a)L1(b)
-        'M2L2X': Assembly(
-            {'M0': 'M', 'M1': 'M', 'L0': 'L', 'L1': 'L', 'X0': 'X'},
-            [('X0.a', 'M0.a'), ('M0.b', 'L0.a'), ('L0.b', 'M1.a'),
-             ('M1.b', 'L1.a')]
         ),
         # M2LX2: X0(a)-(a)M0(b)-(a)L0(b)-(a)M1(b)-(a)X1
         'M2LX2': Assembly(
             {'M0': 'M', 'M1': 'M', 'L0': 'L', 'X0': 'X', 'X1': 'X'},
             [('X0.a', 'M0.a'), ('M0.b', 'L0.a'), ('L0.b', 'M1.a'),
              ('M1.b', 'X1.a')]
+        ),
+        # M2L2X: X0(a)-(a)M0(b)-(a)L0(b)-(a)M1(b)-(a)L1(b)
+        'M2L2X': Assembly(
+            {'M0': 'M', 'M1': 'M', 'L0': 'L', 'L1': 'L', 'X0': 'X'},
+            [('X0.a', 'M0.a'), ('M0.b', 'L0.a'), ('L0.b', 'M1.a'),
+             ('M1.b', 'L1.a')]
         ),
         # M2L2-ring: //-(a)M0(b)-(a)L0(b)-(a)M1(b)-(a)L1(b)-//
         'M2L2-ring': Assembly(
@@ -54,7 +54,7 @@ def assemblies():
     }
 
 
-def test_basic(components, assemblies):
+def test_M2L2_ring(components, assemblies):
     reactions = {
         '1f': Reaction(
             'MX2', 'L', 'MLX', 'X',
@@ -69,9 +69,33 @@ def test_basic(components, assemblies):
             'ML2', 'X', 'MLX', 'L',
             metal_bs='M0.a', leaving_bs='L0.b', entering_bs='X0.a'),
         '3f': Reaction(
+            'MX2', 'MLX', 'M2LX2', 'X',
+            metal_bs='M0.a', leaving_bs='X0.a', entering_bs='L0.a'),
+        '3b': Reaction(
+            'M2LX2', 'X', 'MX2', 'MLX',
+            metal_bs='M0.b', leaving_bs='L0.a', entering_bs='X0.a'),
+        '4f': Reaction(
+            'MLX', 'MLX', 'M2L2X', 'X',
+            metal_bs='M0.b', leaving_bs='X0.a', entering_bs='L0.a'),
+        '4b': Reaction(
+            'M2L2X', 'X', 'MLX', 'MLX',
+            metal_bs='M1.a', leaving_bs='L0.b', entering_bs='X0.a'),
+        '5f': Reaction(
+            'MX2', 'ML2', 'M2L2X', 'X',
+            metal_bs='M0.a', leaving_bs='X0.a', entering_bs='L0.a'),
+        '5b': Reaction(
+            'M2L2X', 'X', 'MX2', 'ML2',
+            metal_bs='M0.b', leaving_bs='L0.a', entering_bs='X0.a'),
+        '6f': Reaction(
+            'M2LX2', 'L', 'M2L2X', 'X',
+            metal_bs='M0.a', leaving_bs='X0.a', entering_bs='L0.a'),
+        '6b': Reaction(
+            'M2L2X', 'X', 'M2LX2', 'L',
+            metal_bs='M1.b', leaving_bs='L1.a', entering_bs='X0.a'),
+        '7f': Reaction(
             'M2L2X', None, 'M2L2-ring', 'X',
             metal_bs='M0.a', leaving_bs='X0.a', entering_bs='L1.b'),
-        '3b': Reaction(
+        '7b': Reaction(
             'M2L2-ring', 'X', 'M2L2X', None,
             metal_bs='M0.b', leaving_bs='L0.a', entering_bs='X0.a'),
         }
@@ -83,6 +107,14 @@ def test_basic(components, assemblies):
         '2b': '2f',
         '3f': '3b',
         '3b': '3f',
+        '4f': '4b',
+        '4b': '4f',
+        '5f': '5b',
+        '5b': '5f',
+        '6f': '6b',
+        '6b': '6f',
+        '7f': '7b',
+        '7b': '7f',
         }
     assert result == expected
 
