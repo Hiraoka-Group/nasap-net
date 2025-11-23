ef434f8ebe8ff35b0f021381c10255b368cf7ece
Keisuke Takeuchi <142223934+takeuchi-sci@users.noreply.github.com>
2025-11-23 15:32:50 +0900

    [typing] default_if_none の型ヒントを修正

--- src/nasap_net/utils/default_if_none.py
+++ src/nasap_net/utils/default_if_none.py
@@ -1,7 +1,7 @@
 from typing import TypeVar
 
-T = TypeVar("T")
+T = TypeVar('T')
 
-def default_if_none(value: T | None, default: T) -> T:
+def default_if_none(value: T, default: T) -> T:
     """Return the value if it is not None, otherwise return the default."""
     return value if value is not None else default
