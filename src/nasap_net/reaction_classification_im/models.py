2d788084254ea3d5f4df636624a7555d09e765a4
Keisuke Takeuchi <142223934+takeuchi-sci@users.noreply.github.com>
2025-11-30 21:39:53 +0900

    init_metal_count_on_ligand と init_ligand_count_on_metal プロパティを作成

--- src/nasap_net/reaction_classification_im/models.py
+++ src/nasap_net/reaction_classification_im/models.py
@@ -1,7 +1,8 @@
 from functools import cached_property
 from typing import Self
 
-from nasap_net.models import Reaction
+from nasap_net.models import Assembly, Reaction
+from . import get_connection_count_of_kind
 from .ring_breaking_size import get_min_breaking_ring_size
 from .ring_formation_size import get_min_forming_ring_size
 
@@ -23,8 +24,7 @@ class ReactionToClassify(Reaction):
     @property
     def entering_kind(self) -> str:
         """Return the kind of the entering binding site."""
-        assem = self.init_assem if self.is_intra() else self.entering_assem_strict
-        return assem.get_component_kind_of_site(self.entering_bs)
+        return self.assem_with_entering_bs.get_component_kind_of_site(self.entering_bs)
 
     def forms_ring(self) -> bool:
         """Whether this reaction forms any rings."""
@@ -47,6 +47,31 @@ class ReactionToClassify(Reaction):
         or None if no rings are broken.
         """
         return get_min_breaking_ring_size(self)
+
+    @property
+    def init_ligand_count_on_metal(self) -> int:
+        """Return the number of ligands bound to the metal before reaction."""
+        return get_connection_count_of_kind(
+            assembly=self.init_assem,
+            source_component_id=self.metal_bs.component_id,
+            target_kind=self.entering_kind,
+        )
+
+    @property
+    def init_metal_count_on_ligand(self) -> int:
+        """Return the number of metals bound to the entering ligand before reaction."""
+        return get_connection_count_of_kind(
+            assembly=self.assem_with_entering_bs,
+            source_component_id=self.entering_bs.component_id,
+            target_kind=self.metal_kind,
+        )
+
+    @property
+    def assem_with_entering_bs(self) -> Assembly:
+        """Return the assembly that contains the entering binding site."""
+        if self.is_inter():
+            return self.entering_assem_strict
+        return self.init_assem
 
     @classmethod
     def from_reaction(cls, reaction: Reaction) -> Self:
